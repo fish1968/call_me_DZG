50aa49e3a49c3dd62a357685fdcf3178be62ad6a
Yabin CHENG <119010046@link.cuhk.edu.cn>
2022-12-12 21:58:57 +0800

    修改了读写过程，除了第一次读取外，都是从内存读取
    但是速度好像很慢，两秒一张图

--- 宴会傻瓜脚本/test.py
+++ 宴会傻瓜脚本/test.py
@@ -9,10 +9,14 @@ from ctypes import *
 import cv2
 import sys
 import imageio
+import numpy as np
 # import keypad
 # import ddt
 import datetime
 #----All refferred to a CSDN Blog: https://blog.csdn.net/luoyir1997/article/details/119117168
+
+import logging
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.INFO)
 
 
 class POINT(Structure):
@@ -55,7 +59,7 @@ class ImageTest:
     #定义APP启动
     def START_APP(self, cmd):
         os.system('START /b %s' % cmd)
-        time.sleep(3)
+        time.sleep(1)
         print('*************start successfully!**************')
     
     #单击图片
@@ -66,7 +70,6 @@ class ImageTest:
     
     def starttest(self):
         self.START_APP(r"C:\Users\97738\Desktop\叫我大掌柜.lnk")
-        time.sleep(1)
  
  
 #   if s<0.9:
@@ -76,7 +79,7 @@ class ImageTest:
  
 def click_picture(picture):
     '''单击图片'''
-    base_path = os.path.join(os.getcwd(), picture)
+    base_path = os.path.join(picture)
     im_screen = ImageGrab.grab()  # 保存
     im_screen.save(r'Test.png')
     source = cv2.imread(r'Test.png')
@@ -90,21 +93,68 @@ def click_picture(picture):
         # print(picture)
         print("执行点击操作：点击",picture,"成功！，坐标：(",x,",",y,")  accuracy = ", s)
         mouse_click(x,y)
+        if picture == "1.jpg":
+            logging.info("当前时间是 {0}，现在识别到了图片1，准确率是 {1}".format(datetime.datetime.now(), s))
         time.sleep(0.1)
     else:
-        print(f"未识别出目标 {picture}, accuracy = {s}")
+        print(f"未识别出目标 {picture}, accuracy = {s}", datetime.datetime.now())
+
+def click_pictures(pictures=[], targets=[]):
+    if len(targets) == 0:
+        for picture in pictures:
+            base_path = os.path.join(picture)
+            template = cv2.imread(base_path)
+            targets.append(template)
+            print("reading")
+    term = 0
+    for picture in pictures:
+        '''单击图片'''
+        im_screen = ImageGrab.grab()  # 保存
+        # im_screen.save(r'Test.png')
+        source = np.array(im_screen.getdata(), dtype ='uint8').reshape((im_screen.size[1], im_screen.size[0], 3))
+        # source = cv2.imread(r'Test.png')
+        # base_path = os.path.join(picture)
+        # template = cv2.imread(base_path)
+        template = targets[term]
+        term += 1
+        result = cv2.matchTemplate(source, template, cv2.TM_CCOEFF_NORMED)
+        # print(result)
+        pos_start = cv2.minMaxLoc(result)[3]
+        x = int(pos_start[0]) + int(template.shape[1] / 2)
+        y = int(pos_start[1]) + int(template.shape[0] / 2)
+        s = cv2.minMaxLoc(result)[1]  # 测试两幅图像精确度
+        if s >= 0.8:
+            # print(picture)
+            print("执行点击操作：点击",picture,"成功！，坐标：(",x,",",y,")  accuracy = ", s)
+            mouse_click(x,y)
+            # time.sleep(0.5)
+            mouse_click(x,y)
+            # time.sleep(0.5)
+            mouse_click(x,y)
+            if picture == "1.jpg":
+                logging.info("当前时间是 {0}，现在识别到了图片1，准确率是 {1}".format(datetime.datetime.now(), s))
+            elif '2' in picture:
+                time.sleep(1)
+            # time.sleep(0.1)
+        else:
+            print(f"未识别出目标 {picture}, accuracy = {s}", datetime.datetime.now())
+            # time.sleep(1)
 
 
 
 if __name__ =='__main__':
     ImageTest().starttest()#启动软件
     #最大化软件
+    pictures = ['1.jpg','2.jpg','3.jpg', '4.jpg', '5.jpg', '6.jpg']
+    targets = []
+    for picture in pictures:
+        base_path = os.path.join(picture)
+        template = cv2.imread(base_path)
+        targets.append(template)
+        logging.info("Reading source image " + str(base_path))
+        
     while True:
-        click_picture('1.jpg')
-        click_picture('2.jpg')
-        click_picture('3.jpg')
-        click_picture('4.jpg')
-        click_picture('5.jpg')
+        click_pictures(pictures=pictures, targets=targets)
         
 #   click_picture('picture6.png')
 

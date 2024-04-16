9bfb58080ec66703fc0db51f342040d20fa0c84f
Yabin CHENG <119010046@link.cuhk.edu.cn>
2024-04-17 05:58:52 +0800

    取消追踪本地文件

--- 宴会傻瓜脚本/local_data.py
+++
@@ -1,8 +0,0 @@
-小程序启动路径 = r"C:\Users\YabinLabtop\Desktop\叫我大掌柜.lnk"
-
-# commands = [1, 10, 9, 1] 
-# commands = [0] # 点击钱庄
-# commands = [9] # 去财神庙点赞
-# commands = [10] # 排行榜点赞
-# commands = [10, 9] # 点赞排行榜，财神庙点赞
-commands = [11] # 商战

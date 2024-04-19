67ee32304b36891076682fc2dc56be764510f84c
Yabin CHENG <119010046@link.cuhk.edu.cn>
2024-04-20 01:29:24 +0800

    Init adb version

---
+++ 宴会傻瓜脚本/ADB_control_version/resources_1080_1920/home/home_data.py
@@ -0,0 +1,28 @@
+# 1080 * 1980
+home_bar = {
+    "home_home":        (100, 1850),
+    "home_shang-pu":    (280, 1850),
+    "home_men-ke":      (460, 1850),
+    "home_chaung-dang": (640, 1850),
+    "home_cheng-jiao":  (800, 1850),
+    "home_bei-bao":     (960, 1850),
+}
+
+home_upper_list = {
+    "delta": (160, 160),
+    "home_xian-shi-jiang-li": (100, 300),
+    "home_xian-shi-chong-zhi": (280, 300),
+    "home_kua-fu-huo-dong": (460, 300),
+}
+
+home_right_low_list = {
+    "delta": (0, 150),
+    "home_ri-chang": (1000, 1320),
+    "home_cheng-jiu": (1000, 1470),
+    "home_cheng-jiu": (1000, 1620),
+    "home_xiao-yu": (1050, 1100)
+}
+
+home_ri_chang_ren_wu = {
+    "ling-qu": (860, 510)
+}
---
+++ 宴会傻瓜脚本/ADB_control_version/test.py
@@ -0,0 +1,25 @@
+import subprocess
+import re
+import time
+import resources_1080_1920.home.home_data
+
+def click_once(x = 0, y = 0):
+    
+    adb_command = ["adb", "shell", "input", "tap", str(x), str(y)]
+    subprocess.Popen(adb_command, stdout=subprocess.PIPE)
+
+
+def click_qian_zhuang_from_home(times = 100):
+    x, y = resources_1080_1920.home_bar.home_data.home_bar["home_shang-pu"]
+    subprocess.Popen(["adb", "shell", "input", "tap", str(x), str(y)])
+    time.sleep(5)
+    x,y = 300, 666
+    for _ in range(times):
+        subprocess.Popen(["adb", "shell", "input", "tap", str(x), str(y)])
+        time.sleep(0.1)
+
+start = time.time()
+click_qian_zhuang_from_home(100)
+
+end = time.time()
+print(end-start)

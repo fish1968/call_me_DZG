4fb2fe5a95200246c3873cd5014b275094f7d250
Yabin CHENG <119010046@link.cuhk.edu.cn>
2024-04-24 06:57:53 +0800

    懒狗，还是上传了吧，无所谓

---
+++ 宴会傻瓜脚本/ADB_control_version/local_data.py
@@ -0,0 +1,6 @@
+# just for my debug
+local_device = "localhost:5555"
+# my debug part end
+debugging = True
+
+apk_start_path = r"C:\Users\YabinLabtop\Desktop\叫我大掌柜H5.lnk"
---
+++ 宴会傻瓜脚本/local_data.py
@@ -0,0 +1,10 @@
+# 此文件的更改不会被 git 追踪
+小程序启动路径 = r"C:\Users\YabinLabtop\Desktop\叫我大掌柜.lnk"
+
+# commands = [1, 10, 9, 1] 
+# commands = [0] # 点击钱庄
+# commands = [9] # 去财神庙点赞
+# commands = [10] # 排行榜点赞
+# commands = [10, 9] # 点赞排行榜，财神庙点赞
+#commands = [11] # 商战
+commands = [0, 1, 10, 9, 11] 

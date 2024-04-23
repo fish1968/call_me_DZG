fd556ea1e25528f9fdc42d9320a4f781740ce118
Yabin CHENG <119010046@link.cuhk.edu.cn>
2024-04-24 02:35:36 +0800

    update test file, daily job update

--- 宴会傻瓜脚本/ADB_control_version/test.py
+++ 宴会傻瓜脚本/ADB_control_version/test.py
@@ -7,9 +7,8 @@ from local_data import local_device
 
 def need_test():
     obtain_screenshot()
-    tu_di_raise_up(device=local_device) # 弹窗 导致卡死
-    start_apk_game()
-    
+    tu_di_raise_up(device=local_device) # 弹窗——导致卡死， 需要修改，现在有一键培养
+    start_apk_game() # 容易点到特权
 
 def passed_test():
     click_qian_zhuang_from_home(100)
@@ -33,10 +32,30 @@ def passed_test():
     ri_chang_ren_wu_qian_zhuang_20()
     daily_xing_yun_duo_bao_2()
 
+def test_yu_gan():
+    # 鱼竿测试，发现有偏移现象
+    st = 1.3
+    for _ in range(10):
+        print(f"st = {st}")
+        daily_ling_qu_yu_gan(sleep_time = st)
+        st -= 0.1
+
 def daily_once(device = local_device):
     # 启动
-    start_apk_game()
-    # 身份元宝
+    # start_apk_game() # 容易卡住
+    enter_chuang_dang(device=device, sleep_time=6)
+    enter_cheng_jiao(device=device, sleep_time=6)
+    enter_shang_pu(device=device, sleep_time=6)
+    enter_home(device=device, sleep_time=6)
+    # 行善
+    daily_xing_shan(device=device)
+    # 日常邮件
+    daily_mail_process(device=device)
+    # 商会建设
+    click_union_basic_constrcut(device=device)
+    # 商战
+    shang_zhan(device=device)
+    # 身份元宝领取
     daily_profile_yuan_bao(device=device,sleep_time=1)
     # 幸运夺宝
     daily_xing_yun_duo_bao_2(device=device) # 貌似点不到？
@@ -47,11 +66,11 @@ def daily_once(device = local_device):
     # 日常钱庄委任更换
     daily_click_qian_zhuang_wei_ren(device=device, sleep_time=1)
     # 乔迁点赞
-    daily_qiao_qian(device=device, sleep_time=1)
+    daily_qiao_qian(device=device, sleep_time=0.8)
     # 排行榜点赞与上门点赞
     daily_click_rank(device=device, sleep_time=1)
     # 财神庙点赞
-    daily_cai_shen_miao_like(device=device, sleep_time=1)
+    daily_cai_shen_miao_like(device=device, sleep_time=0.8)
     # 日常闯荡一次
     ri_chang_chuang_dang(device=device, sleep_time=1)
     # 城郊游历
@@ -62,12 +81,7 @@ def daily_once(device = local_device):
     daily_do_yi_guan()
     daily_do_jiu_si()
     daily_do_yao_pu()
-    # 商会建设
-    click_union_basic_constrcut(device=device)
-    # 商战
-    shang_zhan(device=device)
-    # 行善
-    daily_xing_shan(device=device)
+    # 等待
     for _ in range(3):
         for _ in range(10):
             time.sleep(60)
@@ -80,9 +94,12 @@ def daily_once(device = local_device):
     click_painless(device=device, times=10)
     shang_zhan(device=device)
 
+
 start = time.time()
+daily_once(device=local_device)
+# daily_mail_process()
+# daily_cai_shen_miao_like(sleep_time=0.6)
 # daily_cheng_jiao_you_li()
-# daily_once(device=local_device)
 # click_qian_zhuang_from_home(30)
 # daily_click_rank()
 # tu_di_raise_up(device=local_device)

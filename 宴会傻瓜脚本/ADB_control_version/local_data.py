f3348f587de281897b6594dc012b84df66609d24
yabin <119010046@link.cuhk.edu.cn>
2024-04-24 23:27:14 +0800

    update nothing

--- 宴会傻瓜脚本/ADB_control_version/local_data.py
+++ 宴会傻瓜脚本/ADB_control_version/local_data.py
@@ -6,4 +6,6 @@ do_xing_shan = False
 # my debug part end
 debugging = True
 
-apk_start_path = r"C:\Users\YabinLabtop\Desktop\叫我大掌柜H5.lnk"
+basic_path = r"C:\Users\YabinLabtop\Desktop" 
+app_name = r"\叫我大掌柜H5.lnk"
+apk_start_path = basic_path + app_name

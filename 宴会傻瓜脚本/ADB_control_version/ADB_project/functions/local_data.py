4463d61f7bcbfd604a659fa60a767e940dee4305
Yabin CHENG <119010046@link.cuhk.edu.cn>
2024-05-06 05:24:55 +0800

    添加一下另一台电脑的路径，这里不太好

--- 宴会傻瓜脚本/ADB_control_version/ADB_project/functions/local_data.py
+++ 宴会傻瓜脚本/ADB_control_version/ADB_project/functions/local_data.py
@@ -1,5 +1,5 @@
 # just for my debug
-local_device = "localhost:5555"
+local_device = "localhost:5556"
 # package name 
 game_package_name = r"com.sqwh5.ys.jwdzg.jwdzgh5_ptzy_ysf37/com.gamesdk.h5.GameActivity"
 quick_fox_package_name = r"com.zx.a2_quickfox"
@@ -13,4 +13,5 @@ adb_debugging = False
 basic_path = r"C:\Users\YabinLabtop\Desktop" 
 app_name = r"\叫我大掌柜H5.lnk"
 apk_start_path = basic_path + app_name
-emulator_path = r"D:\LDPlayer\LDPlayer9\dnplayer.exe"
+# emulator_path = r"D:\LDPlayer\LDPlayer9\dnplayer.exe"
+emulator_path = r"C:\Program Files\BlueStacks_nxt\HD-Player.exe"

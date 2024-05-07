99a2f4f0302c7d83470719554f2f5e6f0074fb72
yabin <119010046@link.cuhk.edu.cn>
2024-05-08 05:26:48 +0800

    执行完，点击 exit，避免卡死在一个地点，检查下 men_sheng_raise_up 卡死在四点

--- 宴会傻瓜脚本/ADB_control_version/ADB_project/scripts/routine.py
+++ 宴会傻瓜脚本/ADB_control_version/ADB_project/scripts/routine.py
@@ -9,6 +9,7 @@ from ADB_project.functions.cheng_jiao_functions import xiang_mu_zhao_shang, shan
 from ADB_project.functions.events_functions import da_long, shou_lie, quick_fox
 from ADB_project.functions.home_functions import zhi_you_tan_xin, men_sheng_raise_up
 from ADB_project.functions.shang_pu_functions import daily_do_jiu_si, daily_do_yao_pu
+from ADB_project.functions.basic_dzg_functions import click_exit
 if __name__ == "__main__":
     init_begin = time.time()
     # if current_acitivity is not dzg
@@ -43,8 +44,10 @@ if __name__ == "__main__":
         if t_hour == 0 and has_do_once == False:
             daily_do_once(device = device, sleep_time= 2)
             daily_do_once = True
+            click_exit(device= device, sleep_time=0.3, times = 10)
         if t_hour == 20 and (has_da_long == False):
             da_long(device = device, sleep_time=2)
+            click_exit(device= device, sleep_time=0.3, times = 10)
             has_da_long = True
         if (t_hour in [12, 13]): # need improvement
             shou_lie(device = device, sleep_time=2)
@@ -52,13 +55,16 @@ if __name__ == "__main__":
             if has_seats == False:
                 xiang_mu_zhao_shang(device = device, sleep_time= 2)
                 has_seats = True
+                click_exit(device= device, sleep_time=0.3, times = 10)
         else:
             has_seats = False
         if t_hour in [3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23]:
             if (did_shang_zhan_this_hour == False):
                 shang_zhan(device = device, sleep_time=2)
                 did_shang_zhan_this_hour = True
+                click_exit(device= device, sleep_time=0.3, times = 10)
             click_union_basic_constrcut(device = device, sleep_time= 3)
+            click_exit(device= device, sleep_time=0.3, times = 10)
         else:
             did_shang_zhan_this_hour = False
         
@@ -66,24 +72,28 @@ if __name__ == "__main__":
             if (has_tan_xin == False):
                 zhi_you_tan_xin(device = device, sleep_time=2)
                 has_tan_xin = True
+                click_exit(device= device, sleep_time=0.3, times = 10)
         else:
             has_tan_xin = False
         if t_hour in [3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23]:
             if (has_you_li== False):
                 daily_cheng_jiao_you_li(device = device, sleep_time=2)
                 has_you_li = True
+                click_exit(device= device, sleep_time=0.3, times = 10)
         else:
             has_you_li = False
         if t_hour in [4, 7, 11, 14, 18, 21]:
             if (did_men_sheng_this_hour == False):
                 men_sheng_raise_up(device = device, sleep_time = 2)
                 did_men_sheng_this_hour = True
+                click_exit(device= device, sleep_time=0.3, times = 10)
             else:
                 did_men_sheng_this_hour = False
         if t_hour in [10, 15, 21]:
             if (did_yu_gan_this_hour == False):
                 daily_ling_qu_yu_gan(device = device, sleep_time = 2)
                 did_yu_gan_this_hour = True
+                click_exit(device= device, sleep_time=0.3, times = 10)
         else:
             did_yu_gan_this_hour = False
         if t_hour in [11, 16, 22]:
@@ -91,6 +101,8 @@ if __name__ == "__main__":
                 daily_do_jiu_si(device = device, sleep_time = 2)
                 daily_do_yao_pu(device = device, sleep_time = 2)
                 did_jiu_si_and_yao_pu_this_hour = True
+                click_exit(device= device, sleep_time=0.3, times = 10)
         else:
             did_jiu_si_and_yao_pu_this_hour = False
+        click_exit(device= device, sleep_time=0.3, times = 10)
         click_wait(total_time = 100, sleep_time = 10, device = local_device)
